3fbb54af74baad34811695a4e14f07359eb0ea5e
S518729 <S518729@NW23971.nwmissouri.edu>
2014-04-03 14:49:33 -0500

    Rajesh Assignment

--- src/githubassignment/GitHubAssignment.java
+++ src/githubassignment/GitHubAssignment.java
@@ -20,5 +20,6 @@ public class GitHubAssignment {
         System.out.println("Manasa Manne");
         System.out.println("Swathi Moosaramthota");
         System.out.println("Sravyatha Vegesna");
+        System.out.println("Rajesh assignment");
     }
 }
